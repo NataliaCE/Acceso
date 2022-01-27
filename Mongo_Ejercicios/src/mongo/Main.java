9f8f037c6810a7dc1d15c98cf0b98f2301fc3e96
Natalia Canudas <natalia.200351@hotmail.com>
2022-01-27 10:15:42 +0100

    27 ENE

--- Mongo_Ejercicios/src/mongo/Main.java
+++ Mongo_Ejercicios/src/mongo/Main.java
@@ -1,10 +1,79 @@
 package mongo;
 
+import java.util.Iterator;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class Main {
 
 	public static void main(String[] args) {
-			
+		
+		//Crear cliente
+		MongoClient mongo = new MongoClient("localhost", 27017);
+		System.out.println("Conectado a base de datos.");
+		
+		//Acceder a la base de datos
+		MongoDatabase db = mongo.getDatabase("test");
+		
+		//Acceder a la colección
+		MongoCollection<Document> coleccion = db.getCollection("ciudades");
+		System.out.println("Colección seleccionada.");
+		
+		Ciudad huesca = new Ciudad();
+		huesca.setName("Huesca");
+		huesca.setCountry("ES");
+		huesca.setPopulation((long) 54083);
 
+		//insertaCiudad(huesca, coleccion);
+		//listarCiudades(coleccion);
+		listarCiudadesPais("ES", coleccion);
+		
+		mongo.close();
+	}
+	
+	public static boolean insertaCiudad(Ciudad ciudad, MongoCollection<Document> coleccion) {
+		//Crear documento
+		Document documento = new Document("name", ciudad.getName())
+				.append("country", ciudad.getCountry())
+				.append("timezone", ciudad.getTimezone())
+				.append("location", new Document("longitude", ciudad.getLongitude())
+						.append("latitude", ciudad.getLatitude()))
+				.append("population", ciudad.getPopulation());
+		
+		//Insertar documento
+		try {
+			coleccion.insertOne(documento);
+			System.out.println("Documento insertado");
+			return true;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static void listarCiudades(MongoCollection<Document> coleccion) {
+		//Busqueda
+		FindIterable<Document> iterDoc = coleccion.find();
+		
+		//Iterador
+		Iterator<Document> it = iterDoc.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next().getString("name"));
+		}
+	}
+	
+	public static void listarCiudadesPais(String pais, MongoCollection<Document> coleccion) {
+		Document criterios = new Document("country", pais);
+		FindIterable find = coleccion.find(criterios);
+		Iterator<Document> it = find.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next().getString("name"));
+		}
 	}
 
 }
